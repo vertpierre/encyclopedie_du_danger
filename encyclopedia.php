6590cd812537a97d9ee52c4f8096423b21b8fab4
pierrers-design <roussel.pierre@protonmail.com>
2023-02-14 12:05:33 +0100

    Corrected version, optimized for speed
    New version updated with bugs corrected when loading images and pdfs.

--- encyclopedia.php
+++ encyclopedia.php
@@ -2,12 +2,13 @@
 <html lang="fr">
 
 <head>
-  <meta charset="UTF-8" />
-  <meta http-equiv="X-UA-Compatible" content="IE=edge" />
-  <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-  <link href="style/normalize.css" rel="stylesheet" />
-  <link href="style/simplebar.css" rel="stylesheet" />
-  <link href="style/style.css" rel="stylesheet" />
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <title>une encyclopédie du danger</title>
+  <link href="style/normalize.css" rel="stylesheet">
+  <link href="style/simplebar.css" rel="stylesheet">
+  <link href="style/style.css" rel="stylesheet">
   <noscript>
     <style>
       [data-simplebar] {
@@ -15,168 +16,67 @@
       }
     </style>
   </noscript>
-  <title>une encyclopédie du danger</title>
 </head>
 
 <body>
+  <?php
+  $categoriesPath = __DIR__ . "/import/categories";
+  $projects = glob("$categoriesPath/*", GLOB_ONLYDIR);
+  ?>
   <div class="container-global">
     <nav>
       <header>
-        <h1>
-          <a href="index.html"><i>🡐 retour index</i></a>
-        </h1>
+        <h1><a href="./index.html"><i>retour index</i></a></h1>
       </header>
-
       <div data-simplebar class="liste">
         <ul>
-          <li>
-            <p><a href="#mort">la mort</a></p>
-          </li>
-          <li>
-            <p><a href="#guerre">la guerre</a></p>
-          </li>
-          <li>
-            <p><a href="#religion">la religion</a></p>
-          </li>
-          <li>
-            <p><a href="#enfer">les enfers</a></p>
-          </li>
-          <li>
-            <p><a href="#feu">le feu</a></p>
-          </li>
-          <li>
-            <p><a href="#catastrophe-naturelle">les catastrophes naturelles</a></p>
-          </li>
-          <li>
-            <p><a href="#cri">le cri</a></p>
-          </li>
-          <li>
-            <p><a href="#monstre">les monstres</a></p>
-          </li>
-          <li>
-            <p><a href="#sommeil">le sommeil</a></p>
-          </li>
-          <li>
-            <p><a href="#juste-derriere">juste derrière</a></p>
-          </li>
-          <li>
-            <p><a href="#echelle">l'échelle</a></p>
-          </li>
-          <li>
-            <p><a href="#arme">l'arme</a></p>
-          </li>
-          <li>
-            <p><a href="#masque">le masque</a></p>
-          </li>
-          <li>
-            <p><a href="#fuite">la fuite</a></p>
-          </li>
-          <li>
-            <p><a href="#chute">la chute</a></p>
-          </li>
-          <li>
-            <p><a href="#volcan">le volcan</a></p>
-          </li>
-          <li>
-            <p><a href="#maladie">la maladie</a></p>
-          </li>
-          <li>
-            <p><a href="#insoupconne">l'insoupçonné</a></p>
-          </li>
-          <li>
-            <p><a href="#explosion">les explosions</a></p>
-          </li>
-          <li>
-            <p><a href="#famille">la famille</a></p>
-          </li>
-          <li>
-            <p><a href="#en-joue">en joue</a></p>
-          </li>
-          <li>
-            <p><a href="#predateur">le prédateur</a></p>
-          </li>
-          <li>
-            <p><a href="#poison">le poison</a></p>
-          </li>
-          <li>
-            <p><a href="#lame">la lame</a></p>
-          </li>
-          <li>
-            <p><a href="#etre-vu">être vu</a></p>
-          </li>
-          <li>
-            <p><a href="#hors-champ">le hors-champ</a></p>
-          </li>
-          <li>
-            <p><a href="#regard">le regard</a></p>
-          </li>
-          <li>
-            <p><a href="#surveillance">la surveillance</a></p>
-          </li>
-          <li>
-            <p><a href="#porte">derrière la porte</a></p>
-          </li>
-          <li>
+          <?php foreach ($projects as $project) {
+            $imgdir = "$project/images/";
+            $images = glob("$imgdir{*.jpg,*.jpeg,*.gif,*.png,*.svg}", GLOB_BRACE);
+            $projectName = preg_replace("/[0-9]+ /", "", str_replace("_", " ", str_replace("l_", "l'_", basename($project))));
+          ?>
+            <li>
+              <p><em><a href="#<?= $projectName ?>"><?= $projectName ?></a></em></p>
+            </li>
+          <?php } ?>
         </ul>
       </div>
     </nav>
-
     <div data-simplebar class="container-galleries">
-      <?php
-
-      // Include Libs and Functions
-      require_once "spyc.php";
-      require_once "functions.php";
-
-      // New yaml instance
-      $spyc = new Spyc();
-
-      // Recursively read the chosen directory for yaml content in .md
-      $directory = "import/categories/";
-      $ignored = "import/categories/README.md";
-      $projects = rglob($directory . "{*.md}", GLOB_BRACE);
-      $projectNbr = 0;
-
-      // Sort projects, most recent first
-      usort($projects, create_function('$a,$b', 'return filemtime($b) - filemtime($a);'));
-
-      foreach ($projects as $project) {
-        $projectNbr++;
-        if ($project == $ignored) {
-          return;
-        }
-        $y = Spyc::YAMLLoad($project);
+      <?php foreach ($projects as $project) {
+        $imgdir = "$project/images/";
+        $images = glob("$imgdir{*.jpg,*.jpeg,*.gif,*.png,*.svg}", GLOB_BRACE);
+        $projectName = preg_replace("/[0-9]+ /", "", str_replace("_", " ", str_replace("l_", "l'_", basename($project))));
       ?>
         <section class="category">
-          <div id="<?php echo $y['place_anchor']; ?>">
-            <h3><em><?php echo $y['place_name']; ?></em></h3>
+          <div id="<?= $projectName ?>">
+            <h3><em><?= $projectName ?></em></h3>
           </div>
           <div data-simplebar class="container-scroll-x">
             <div class="container-images">
               <?php
-              $imgdir = dirname($project) . "/images/"; // need to be polished
-              $images = glob($imgdir . "{*.jpg,*.jpeg,*.gif,*.png,*.svg}", GLOB_BRACE);
-              $arrayPosJustify = array("flex-start", "center", "flex-end");
-              $arrayPosAlign = array("flex-start", "center", "flex-end");
+              $arrayPosJustify = ["flex-start", "center", "flex-end"];
+              $arrayPosAlign = ["flex-start", "center", "flex-end"];
               foreach ($images as $image) {
                 $randPosJustify = array_rand($arrayPosJustify, 1);
                 $randPosAlign = array_rand($arrayPosAlign, 1);
-                echo "<div style='justify-content:$arrayPosJustify[$randPosJustify];align-items:$arrayPosAlign[$randPosAlign];' class='images'><img src='$image' /></div>";
+                $imagePath = str_replace(__DIR__, '.', $image);
+                echo "<div style='justify-content:$arrayPosJustify[$randPosJustify];align-items:$arrayPosAlign[$randPosAlign];' class='images'><img src='$imagePath' /></div>";
               }
               ?>
             </div>
           </div>
         </section>
-
       <?php } ?>
-
-      <script src="js/simplebar.js"></script>
-      <script>
-        new SimpleBar(document.getElementById("myElement"));
-      </script>
-      <script src="js/imagesloaded.min.js"></script>
-      <script src="js/magnific.js"></script>
-      <script src="js/unveil.js"></script>
+    </div>
+  </div>
+  <script src="js/simplebar.js"></script>
+  <script>
+    new SimpleBar(document.getElementById("myElement"));
+  </script>
+  <script src="js/imagesloaded.min.js"></script>
+  <script src="js/magnific.js"></script>
+  <script src="js/unveil.js"></script>
 </body>
 
 </html>
